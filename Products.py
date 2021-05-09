20bab5494a985067da6cc82f1dc8387b2ac6f6bd
CaptainGamekiller <yann60810@yahoo.com.tw>
2021-05-08 21:41:51 -0700

    Basic

--- Products.py
+++ Products.py
@@ -1,12 +1,34 @@
 # This program let user to input the product and price in the list.
+import os # operating system
+
+# Read the file
 products = []
+if os.path.isfile('Products.csv'): # check the file is existed or not.
+    print('This file is existed!')
+    with open('Products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if 'Product, Price' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print('Those products are in the file: ', products)
+else:
+    print('This file is not existed!')
+
+# Prompt the user and get inputs
 while True:
+    print('--------------------------------------------------------------')
     name = input('Please type the product name or type "q" to quit: ')
     if name == 'q':
         break
     price = input('Please type the product price: ')
-    # way3，Best solution!
+    price = int(price)
     products.append([name, price])
+print('Update the file!')
+print('New products and price are showing below: \n', products)
 
-print(products)
-print('The first product name is ', products[0][0])
+# write to the .csv file
+with open ('Products.csv', 'w', encoding = 'utf-8') as f:
+    f.write('Product, Price \n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
